183b40649855926c0941562b20e37c1e169ae85d
tuongpra112 <101204696+tuongpra112@users.noreply.github.com>
2022-03-09 00:07:32 +0700

    add get_proc

--- Assignment/source_code/src/sched.c
+++ Assignment/source_code/src/sched.c
@@ -24,6 +24,14 @@ struct pcb_t * get_proc(void) {
 	 * [ready_queue] and return the highest priority one.
 	 * Remember to use lock to protect the queue.
 	 * */
+	pthread_mutex_lock(&queue_lock);
+	if (empty(&ready_queue)) {
+		while (!empty(&run_queue)) {
+			enqueue(&ready_queue, dequeue(&run_queue));
+		}
+	}
+	proc = dequeue(&ready_queue);
+	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
