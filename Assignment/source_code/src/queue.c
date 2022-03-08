178eaae36fa6a62ba77f6272b4fd12a67d451506
duchuykg <100073936+duchuykg@users.noreply.github.com>
2022-03-08 10:21:58 +0700

    Update queue.c

--- Assignment/source_code/src/queue.c
+++ Assignment/source_code/src/queue.c
@@ -20,6 +20,26 @@ struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+
+	int max = 0;
+	int max_i = 0;
+
+	if (q->size == 0)
+		return NULL;
+
+	for (int i = 0; i < q->size; ++i){
+		if (q->proc[i]->priority >= max)
+		{
+			max = q->proc[i]->priority;
+			max_i = i;
+		}
+	}
+
+	struct pcb_t * temp_proc = (struct pcb_t *)malloc(sizeof(struct pcb_t));	
+	temp_proc = q->proc[max_i];
+	q->proc[max_i] = q->proc[q->size - 1];
+	q->size = q->size - 1;
+
+	return temp_proc;
 }
 
